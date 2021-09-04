75bbf73ed497da3e1676337f6ac680fd4d77094c
Auxilia Vinnarasi <auxiliavinnarasi@gmail.com>
2021-09-04 11:47:30 +0530

    DB Added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
-const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://auxilia:admin123@cluster0.bg8rf.mongodb.net?retryWrites=true&w=majority";
 const PORT=process.env.PORT || 3000;
 
 app.use(cors({
